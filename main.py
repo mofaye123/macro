d39c6606afb69407e88d9e186c0a925d1922c844
mofaye123 <mofaye@hotcoin.biz>
2026-02-03 12:05:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 # 1. 导入配置和数据引擎
 from config import API_KEY, SERIES_IDS, CSS_STYLE
-from data_engine import get_fred_data
+from data_engine import get_mixed_data
 
 # 2. 导入各个业务模块
 from modules.dashboard import render_dashboard_standalone
